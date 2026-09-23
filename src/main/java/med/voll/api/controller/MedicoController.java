f182e23ccca46c0b0cd57146aa51645f46182661
FVS-MBA <feliven@gmail.com>
2026-09-23 16:00:20 -0300

    usa @GetMapping e @DeleteMapping com id dinâmico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +37,12 @@ public class MedicoController {
         return medicoRepository.findAll(paginacao).map(MedicoReadDto::new);
     }
 
+    @GetMapping("{id}")
+    public String exibirMedico(@PathVariable Long id) {
+        var medico = medicoRepository.findById(id);
+        return medico.isPresent() ? medico.get().toString() : "Não existe";
+    }
+
     @PostMapping
     public Medico cadastrarMedico(@RequestBody @Valid MedicoCreateDto json) {
 
@@ -59,5 +67,11 @@ public class MedicoController {
         var medico = medicoRepository.getReferenceById(json.id());
         medico.atualizarCadastro(json);
         return "👍";
+    }
+
+    @DeleteMapping("{id}")
+    public String excluirMedico(@PathVariable Long id) {
+        medicoRepository.deleteById(id);
+        return "🚮";
     }
 }
