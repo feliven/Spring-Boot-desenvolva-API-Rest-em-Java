1f6a8af2841105bb7d6b82237f6220ec35679848
FVS-MBA <feliven@gmail.com>
2026-09-23 13:26:29 -0300

    implementa @PageableDefault e ativa show-sql

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,8 @@ package med.voll.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +25,8 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @GetMapping
-    public Page<MedicoReadDto> listarMedicos(Pageable paginacao) {
+    public Page<MedicoReadDto> listarMedicos(
+            @PageableDefault(size = 3, sort = { "numeroCrm" }, direction = Direction.DESC) Pageable paginacao) {
 
         return medicoRepository.findAll(paginacao).map(MedicoReadDto::new);
     }
