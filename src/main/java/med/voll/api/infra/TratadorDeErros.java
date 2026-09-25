4679d35aa2f0aa0deaf3d314ae7064d2e50196ab
FVS-MBA <feliven@gmail.com>
2026-09-24 21:29:01 -0300

    trata erro 400

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,8 +1,10 @@
 package med.voll.api.infra;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,4 +15,16 @@ public class TratadorDeErros {
     public ResponseEntity<String> tratarErro404() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<erro400Dto>> tratarErro400(MethodArgumentNotValidException e) {
+        var erros = e.getFieldErrors().stream()
+                .map(fieldError -> new erro400Dto(fieldError.getField(), fieldError.getDefaultMessage())).toList();
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    private record erro400Dto(String field, String defaultMessage) {
+    }
+
 }
